367e5752bc5b5205a7883383382884d2be97ad61
Ballast <birgit.leitner@yahoo.de>
2025-06-24 17:09:26 +0200

    gameComp included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartScreenComponent
+    StartScreenComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
