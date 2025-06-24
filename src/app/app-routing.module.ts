0691ff3694a9a9ddcdf945637882c5d98d85c3db
Ballast <birgit.leitner@yahoo.de>
2025-06-24 17:09:08 +0200

    gameComponent included

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
     { path: '', component: StartScreenComponent},
+    { path: 'game', component: GameComponent}
 ];
 
 @NgModule({
