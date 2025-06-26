139949654d08467453b2fb27a3c1b74e22dce2ff
Ballast <birgit.leitner@yahoo.de>
2025-06-26 13:33:06 +0200

    dialog comp ts

---
+++ src/app/my-dialog/my-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-my-dialog',
+  templateUrl: './my-dialog.component.html',
+  styleUrls: ['./my-dialog.component.scss'],
+})
+export class MyDialogComponent {
+  name: string = '';
+  constructor(
+    public dialogRef: MatDialogRef<MyDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { name: string }
+  ) {}
+
+  onNoClick(){
+    
+  }
+
+  close(): void {
+    this.dialogRef.close("Neuer Wert vom Nutzer");
+  }
+}
