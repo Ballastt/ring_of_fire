1bf9017f246c734f13632d1cdc4008a7ce323ed4
Ballast <birgit.leitner@yahoo.de>
2025-07-11 12:07:44 +0200

    objects added to constructor, new game method defined

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Firestore, collection, addDoc, getDoc } from '@angular/fire/firestore';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.scss'],
 })
-export class StartScreenComponent implements OnInit{
-  constructor(private router: Router) {}
+export class StartScreenComponent implements OnInit {
+  constructor(private router: Router, private firestore: Firestore) {}
 
   ngOnInit(): void {
     // Initiale Logik (falls nötig)
   }
 
   newGame() {
-    //Start game
-    this.router.navigateByUrl('/game');
+    const game = new Game();
+    const gamesRef = collection(this.firestore, 'games');
+    addDoc(gamesRef, game.toJson())
+      .then(async (docRef: any) => {
+        console.log('Neues Spiel gespeichert mit ID:', docRef.id);
+
+        // Dokumentdaten holen
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const gameInfo = docSnap.data();
+          console.log('Spiel-Daten:', gameInfo);
+        } else {
+          console.log('Kein Dokument gefunden!');
+        }
+
+        this.router.navigate(['/game', docRef.id]);
+      })
+      .catch((error) => {
+        console.error('Fehler beim Speichern:', error);
+      });
   }
 }
