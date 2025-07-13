35fa0ef6b668e1c3a711bf8c86411f3999aa339b
Ballast <birgit.leitner@yahoo.de>
2025-07-13 12:21:26 +0200

    new comp with img array

---
+++ src/app/edit-player/edit-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-player',
+  templateUrl: './edit-player.component.html',
+  styleUrls: ['./edit-player.component.scss']
+})
+export class EditPlayerComponent {
+
+  allProfilePictures = ['female_avatar.png', 'profile.webp', 'male.png'];
+
+}
