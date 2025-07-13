1b226fb1fb77b50aed221273bf7ab722487c8c2e
Ballast <birgit.leitner@yahoo.de>
2025-07-13 12:22:23 +0200

    new imports, methods + update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,29 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { MyDialogComponent } from '../my-dialog/my-dialog.component';
 import { ActivatedRoute } from '@angular/router';
-import { Router } from '@angular/router'; 
-import { doc, docData } from '@angular/fire/firestore';
-
-import {
-  Firestore,
-  collection,
-  collectionData,
-  addDoc,
-} from '@angular/fire/firestore';
-
+import { Router } from '@angular/router';
+import { doc, docData, updateDoc, Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { EditPlayerComponent } from '../edit-player/edit-player.component';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
-  pickCardAnimation = false;
-  currentCard: string | undefined = '';
+export class GameComponent implements OnInit {
   game: Game = new Game();
+  gameId: string | undefined = '';
 
   games$!: Observable<any[]>;
 
@@ -34,37 +26,53 @@ export class GameComponent {
     public dialog: MatDialog
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      const gameId = params['id'];
-
-      if (gameId) {
-        const gameDocRef = doc(this.firestore, 'games', gameId);
+      this.gameId = params['id'];
+      console.log('Spiel-ID aus der URL:', this.gameId);
+      if (this.gameId) {
+        const gameDocRef = doc(this.firestore, 'games', this.gameId);
         docData(gameDocRef).subscribe((gameData: any) => {
           this.game = new Game();
           Object.assign(this.game, gameData);
-          console.log('Live aktualisiertes Spiel:', this.game);
+          // Absicherung: aktuelle Werte prüfen und korrekt zuweisen
+          this.game.currentPlayer = Number(this.game.currentPlayer) || 0;
+          this.game.pickCardAnimation = !!this.game.pickCardAnimation;
+          this.game.currentCard = this.game.currentCard || '';
+          this.game.players = this.game.players || [];
+          this.game.stack = this.game.stack || [];
+          this.game.playedCards = this.game.playedCards || [];
+          this.game.playerImages = this.game.playerImages || [];
+          console.log('Aktuelles Spiel geladen:', this.game);
         });
       }
     });
   }
 
-
   pickCard() {
-    if (!this.pickCardAnimation) {
-      this.currentCard = this.game.stack.pop();
-      this.pickCardAnimation = true;
+    console.log('[KLICK] pickCard() aufgerufen');
+    if (!this.game.pickCardAnimation) {
+      console.log('[OK] Animation nicht aktiv → Karte ziehen');
+
+      this.game.currentCard = this.game.stack.pop() || '';
+      this.game.pickCardAnimation = true;
 
       this.game.currentPlayer++;
-      this.game.currentPlayer =
-        this.game.currentPlayer % this.game.players.length;
+      this.game.currentPlayer %= this.game.players.length;
 
       setTimeout(() => {
-        if (this.currentCard) {
-          this.game.playedCards.push(this.currentCard);
+        if (this.game.currentCard) {
+          this.game.playedCards.push(this.game.currentCard);
         }
-        this.pickCardAnimation = false;
+
+        this.game.pickCardAnimation = false;
+        this.game.currentCard = ''; // <- HIER setzen, nachdem sie ins Array ging
+
+        this.saveGame();
+        console.log('[ANIMATION ENDE] Karte gelegt & gespeichert');
       }, 2000);
+    } else {
+      console.log('[BLOCKIERT] Animation läuft noch – kein Ziehen möglich');
     }
   }
 
@@ -75,7 +83,30 @@ export class GameComponent {
       if (name && name.length > 0) {
         this.game.players.push(name);
         console.log('Dialog wurde geschlossen mit:', name);
+        this.saveGame();
       }
+    });
+  }
+
+  saveGame() {
+    const gameRef = doc(this.firestore, 'games', this.gameId || '');
+    updateDoc(gameRef, this.game.toJson())
+      .then(() => {
+        console.log('Spiel aktualisiert:', this.game);
+        // Optional: Navigiere zurück zur Startseite oder führe eine andere Aktion aus
+      })
+      .catch((error) => {
+        console.error('Fehler beim Aktualisieren des Spiels:', error);
+      });
+  }
+
+  editPlayer(playerId: number) {
+    console.log('[KLICK] editPlayer() aufgerufen', playerId);
+
+    const dialogRef = this.dialog.open(EditPlayerComponent);
+    dialogRef.afterClosed().subscribe((change: string) => {
+      console.log("Received Change", change);
+      
     });
   }
 }
